1951c70c1c7ddbd74dff3567cb8902f387440888
Ivan <example@gmail.com>
2025-12-15 18:20:05 +0100

    funktionierende anwendung

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,36 +3,37 @@ sap.ui.define([
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel"
-], 
+],
 
-(Controller, MessageToast, JSONModel, ResourceModel ) => {
-    "use strict";
+    (Controller, MessageToast, JSONModel, ResourceModel) => {
+        "use strict";
 
-    return Controller.extend("ui5_walkthroug.controller.App", {
-        
-        onInit : function () {
-            let oVorname = this.byId("idVorname");
-            oVorname.setEditable(false); 
+        return Controller.extend("ui5_walkthroug.controller.App", {
 
-        },
-           
-        onClickMe() {
-            // read message from i18n model
-            const oBundle = this.getView().getModel("i18n").getResourceBundle()
-            const sTerm = this.getView().getModel().getProperty("/terms/name2");
-            const text = oBundle.getText("helloMsg", [sTerm]);
+            onInit: function () {
+                let oVorname = this.byId("idVorname");
+                oVorname.setEditable(false);
 
-            MessageToast.show(text);
-        },
+            },
 
-        onSelectionChange: function(oEvent) {
-            let oSelectedItem = oEvent.getParameter("listItem");
-            let oContext = oSelectedItem.getBindingContext();
-            let sPath = oContext.getPath();
-            let oForm = this.byId("adressForm");
-            oForm.bindElement(sPath + "/Adresse");
-        },
+            onClickMe() {
+                // read message from i18n model
+                const oBundle = this.getView().getModel("i18n").getResourceBundle()
+                const sTerm = this.getView().getModel().getProperty("/terms/name2");
+                const text = oBundle.getText("helloMsg", [sTerm]);
 
+                MessageToast.show(text);
+            },
 
+            onSelectionChange: function (oEvent) {
+                debugger;
+                    var oSelectedItem = oEvent.getParameter("listItem");
+                    var oBindingContext = oSelectedItem.getBindingContext();
+                    var sPath = oBindingContext.getPath();
+                    var oForm = this.byId("adressForm");
+
+                    oForm.bindElement(sPath + "/Adresse");
+                }
+            
+            });
     });
-});
